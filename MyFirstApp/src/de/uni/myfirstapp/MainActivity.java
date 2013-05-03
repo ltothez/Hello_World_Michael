6cd6869a4f240223ca1ed59161ba8c0b9db9b8c1
Michael Bortlik <Michael.Bortlik@gmx.de>
2013-05-03 15:05:46 +0200

    2te View hinzugefügt

--- MyFirstApp/src/de/uni/myfirstapp/MainActivity.java
+++ MyFirstApp/src/de/uni/myfirstapp/MainActivity.java
@@ -3,9 +3,14 @@ package de.uni.myfirstapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	
+	public final static String EXTRA_MESSAGE = "de.uni.myfirstapp.MESSAGE";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,18 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void sendMessage(View view)
+    {
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editText = (EditText) findViewById(R.id.edit_message);
+    	
+    	String message = editText.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
+    	
+    	startActivity(intent);
+    	
     }
     
 }
